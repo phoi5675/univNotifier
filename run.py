f5637b1d1af67aa6d80ee0fb1aad775e41c7ab4b
강민재 <38933647+phoi5675@users.noreply.github.com>
2020-07-13 18:19:50 +0900

    Add files via upload
    오류 발생 시 log.txt 파일 생성

--- run.py
+++ run.py
@@ -1,10 +1,24 @@
 if __name__ == '__main__':
-    # 항공대학교 공지
-    from webScrap.kau import KauScrapMain, KauSendMail
-    KauScrapMain.main()
-    KauSendMail.main()
+    def saveError(error):
+        from datetime import date
+        today = date.today().isoformat()
+        print(error)
+        file = open('log.txt', 'a')
 
-    # 상명대학교 공지
-    from webScrap.smu import SmuScrapMain, SmuSendMail
-    SmuScrapMain.main()
-    SmuSendMail.main()
+        file.writelines(today + '    ' + repr(error) + '\n\n')
+        file.close()
+    try:
+        # 항공대학교 공지
+        from webScrap.kau import KauScrapMain, KauSendMail
+        KauScrapMain.main()
+        KauSendMail.main()
+    except Exception as error:
+        saveError(error)
+
+    try:
+        # 상명대학교 공지
+        from webScrap.smu import SmuScrapMain, SmuSendMail
+        SmuScrapMain.main()
+        SmuSendMail.main()
+    except Exception as error:
+        saveError(error)
