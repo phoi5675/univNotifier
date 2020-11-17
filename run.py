3425a6d73cf41b44d0b065cbeff7e077a567528a
강민재 <38933647+phoi5675@users.noreply.github.com>
2020-11-17 10:18:13 +0900

    Add files via upload
    실행파일 분리

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 if __name__ == '__main__':
+    # 항공대학교 공지
+    from webScrap.kau import KauScrapMain, KauSendMail
+    KauScrapMain.main()
+
+    # 상명대학교 공지
+    from webScrap.smu import SmuScrapMain, SmuSendMail
+    SmuScrapMain.main()
+    # SmuSendMail.main()
+'''
     def saveError(error):
         from datetime import date
         today = date.today().isoformat()
         print(error)
-        file = open('./log.txt', 'a')
+        file = open('log.txt', 'a')
 
         file.writelines(today + '    ' + repr(error) + '\n\n')
         file.close()
@@ -11,7 +20,7 @@ if __name__ == '__main__':
         # 항공대학교 공지
         from webScrap.kau import KauScrapMain, KauSendMail
         KauScrapMain.main()
-        KauSendMail.main()
+        #KauSendMail.main()
     except Exception as error:
         saveError(error)
 
@@ -19,6 +28,7 @@ if __name__ == '__main__':
         # 상명대학교 공지
         from webScrap.smu import SmuScrapMain, SmuSendMail
         SmuScrapMain.main()
-        SmuSendMail.main()
+        #SmuSendMail.main()
     except Exception as error:
         saveError(error)
+        '''
