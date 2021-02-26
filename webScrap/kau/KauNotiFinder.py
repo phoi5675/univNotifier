0fd24324af0b3cfcdb679e3e76c48f790bfdf0c7
강민재 <38933647+phoi5675@users.noreply.github.com>
2021-02-26 15:56:38 +0900

    Add files via upload

---
+++ webScrap/kau/KauNotiFinder.py
@@ -0,0 +1,158 @@
+# -*- coding:utf-8 -*-
+from webScrap.modules.NotiFinder import *
+
+
+class GenNotiFinder(NotiFinder):
+    def __init__(self):
+        super().__init__()
+        # 공지 검색을 위한 html attr / values
+        self.setAttributeAndValue(attribute='class', value='table_board', select='notiTable')
+        self.setAttributeAndValue(attribute='tag', value='tr', select='notiLine')
+        self.setAttributeAndValue(attribute='class', value='tit', select='title')
+        self.setAttributeAndValue(attribute='class', value='not_m', select='date')
+        self.setAttributeAndValue(attribute='onclick', value='*', select='href')
+
+        # 텍스트에서 불피요한 태그 삭제
+        self.addRemoveTagKeywords(removeTag='ul', select='title')
+
+        # 미리보기 설정
+        self.setAttributeAndValue("id", "divViewConts", "preview")
+
+    def getHref(self, wrappedNotiLine):
+        try:
+            found = wrappedNotiLine.find(
+                attrs={self.notiFinderElements['title'].attrKeyword, self.notiFinderElements['title'].valueKeyword}
+            )
+            href = found.get(self.notiFinderElements['href'].attrKeyword)
+            if href is None:
+                href = found.a.get(self.notiFinderElements['href'].attrKeyword)
+            # 링크 수정은 나중에
+            return href[(href.find(')') - 6): href.find(')') - 1]
+
+        except AttributeError:
+            return ''
+
+    def getTitle(self, wrappedNotiLine):
+        return super().getTitle(wrappedNotiLine)
+
+    def getDate(self, wrappedNotiLine):
+        found = wrappedNotiLine.find_all(
+            attrs={self.notiFinderElements['date'].attrKeyword: self.notiFinderElements['date'].valueKeyword}
+        )
+        if not found:
+            return ""
+        return found[1].get_text()
+
+
+class DeptNotiFinder(NotiFinder):
+    def __init__(self):
+        super().__init__()
+        # 공지 검색을 위한 html attr / values
+        self.setAttributeAndValue(attribute='id', value='notiDfTable', select='notiTable')
+        self.setAttributeAndValue(attribute='tag', value='tr', select='notiLine')
+        self.setAttributeAndValue(attribute='class', value='tit', select='title')
+        self.setAttributeAndValue(attribute='class', value='not_m', select='date')
+        self.setAttributeAndValue(attribute='onclick', value='*', select='href')
+
+        # 텍스트에서 불피요한 태그 삭제
+        self.addRemoveTagKeywords(removeTag='ul', select='title')
+
+        # 미리보기 설정
+        self.setAttributeAndValue(attribute="id", value="divViewConts", select="preview")
+
+    def getHref(self, wrappedNotiLine):
+        try:
+            href = wrappedNotiLine.find("td",
+                                        attrs={self.notiFinderElements['href'].attrKeyword: True})[
+                self.notiFinderElements['href'].attrKeyword]
+            return href[(href.find(',') + 3): href.find(')') - 1]
+        except AttributeError:
+            return ''
+
+    def getTitle(self, wrappedNotiLine):
+        return super().getTitle(wrappedNotiLine)
+
+    def getDate(self, wrappedNotiLine):
+        found = wrappedNotiLine.find(
+            attrs={self.notiFinderElements['date'].attrKeyword: self.notiFinderElements['date'].valueKeyword}
+        )
+        if not found:
+            return ""
+        return found.get_text()
+
+
+class CarNotiFinder(NotiFinder):
+    def __init__(self):
+        super().__init__()
+        # 공지 검색을 위한 html attr / values
+        self.setAttributeAndValue(attribute='class', value='black', select='notiTable')
+        self.setAttributeAndValue(attribute='class', value='tbody', select='notiLine')
+        self.setAttributeAndValue(attribute='class', value='title', select='title')
+        self.setAttributeAndValue(attribute='class', value='reg_date', select='date')
+        self.setAttributeAndValue(attribute='', value='href', select='href')
+
+        # 미리보기 설정
+        self.setAttributeAndValue(attribute="data-role", value="wysiwyg-content", select="preview")
+
+    def getHref(self, wrappedNotiLine):
+        return super().getHref(wrappedNotiLine)
+
+    def getTitle(self, wrappedNotiLine):
+        return super().getTitle(wrappedNotiLine)
+
+    def getDate(self, wrappedNotiLine):
+        return super().getDate(wrappedNotiLine)
+
+
+class SmeNotiFinder(NotiFinder):
+    def __init__(self):
+        super().__init__()
+        # 공지 검색을 위한 html attr / values
+        self.setAttributeAndValue(attribute='class', value='table_board', select='notiTable')
+        self.setAttributeAndValue(attribute='tag', value='tr', select='notiLine')
+        self.setAttributeAndValue(attribute='class', value='tit', select='title')
+        self.setAttributeAndValue(attribute='class', value='date', select='date')
+        self.setAttributeAndValue(attribute='', value='href', select='href')
+
+        # 텍스트에서 불피요한 태그 삭제
+        self.addRemoveTagKeywords(removeTag='ul', select='title')
+
+        # 미리보기 설정
+        self.setAttributeAndValue(attribute="class", value="view_conts", select="preview")
+
+    def getHref(self, wrappedNotiLine):
+        href = super().getHref(wrappedNotiLine)
+        return href.replace('..', '')
+
+    def getTitle(self, wrappedNotiLine):
+        return super().getTitle(wrappedNotiLine)
+
+    def getDate(self, wrappedNotiLine):
+        return super().getDate(wrappedNotiLine)
+
+
+class DormNotiFinder(NotiFinder):
+    def __init__(self):
+        super().__init__()
+        # 공지 검색을 위한 html attr / values
+        self.setAttributeAndValue(attribute='class', value='board_list', select='notiTable')
+        self.setAttributeAndValue(attribute='tag', value='tr', select='notiLine')
+        self.setAttributeAndValue(attribute='headers', value='board_title', select='title')
+        self.setAttributeAndValue(attribute='headers', value='board_create', select='date')
+        self.setAttributeAndValue(attribute='href', value='', select='href')
+
+        # 미리보기 설정
+        self.setAttributeAndValue(attribute="colspan", value="4", select="preview")
+
+    def getHref(self, wrappedNotiLine):
+        try:
+            href = wrappedNotiLine.find("a")[self.notiFinderElements['href'].attrKeyword]
+            return href[(href.find('(') + 1): href.find(',')]
+        except AttributeError:
+            return ''
+
+    def getTitle(self, wrappedNotiLine):
+        return super().getTitle(wrappedNotiLine)
+
+    def getDate(self, wrappedNotiLine):
+        return super().getDate(wrappedNotiLine)
--- webScrap/kau/KauScrapMain.py
+++ webScrap/kau/KauScrapMain.py
@@ -1,16 +1,12 @@
 # -*- coding:utf-8 -*-
 import copy
 
-from webScrap.kau.KauFunc import *
 from webScrap.kau.const import *
+from webScrap.kau.KauNotiMaker import *
+from webScrap.kau.KauNotiFinder import *
 from webScrap.modules.DeleteFile import *
 from webScrap.modules.NotiFinder import *
 from webScrap.modules.NotiMaker import *
-'''
-from DeleteFile import *
-from NotiFinder import *
-from NotiMaker import *
-'''
 
 
 def main():
@@ -20,95 +16,90 @@ def main():
         deleteFileFromDirectory(FILEPATH + "html/", html + ".html")
 
     # -------------------- 공지 리스트 생성 -------------------- #
+    # 학교 공지
     genNotiListAll = list(ExtractedNotiList() for i in range(GENINT))
-    genNotiFinder = NotiFinder()
-
-    deptNotiListAll = list(ExtractedNotiList() for i in range(DEPINT))
-    deptNotiFinder = NotiFinder()
-
-    # 취업공지는 따로 생성 / 호환성을 위해 리스트로 생성
-    careerNotiListAll = list(ExtractedNotiList() for i in range(len(CAREER)))
-    careerNotiFinder = NotiFinder()
-
-    # -------------------- 별도 함수 설정 (optional) -------------------- #
-    genNotiFinder.fixHref = extractGenHref
-    deptNotiFinder.fixHref = extractDeptHref
-    genNotiFinder.extractDate = extractGenDate
-
-    # -------------------- 공지 검색 attribute / value 설정 -------------------- #
-    # 읿반 공지
-    genNotiFinder.setAttributeAndValue('class', 'table_board', 'notiList')
-    genNotiFinder.setAttributeAndValue('tag', 'tr', 'notiLine')
-    genNotiFinder.setAttributeAndValue('class', 'tit', 'title')
-    genNotiFinder.setAttributeAndValue('class', 'not_m', 'date')
-    genNotiFinder.setAttributeAndValue('onclick', '*', 'href')
-
-    # 학과 공지
-    deptNotiFinder.setAttributeAndValue('id', 'notiDfTable', 'notiList')
-    deptNotiFinder.setAttributeAndValue('tag', 'tr', 'notiLine')
-    deptNotiFinder.setAttributeAndValue('class', 'tit', 'title')
-    deptNotiFinder.setAttributeAndValue('class', 'not_m', 'date')
-    deptNotiFinder.setAttributeAndValue('onclick', '*', 'href')
+    genNotiFinder = GenNotiFinder()
 
     # 취업 공지
-    careerNotiFinder.setAttributeAndValue('class', 'black', 'notiList')
-    careerNotiFinder.setAttributeAndValue('class', 'tbody', 'notiLine')
-    careerNotiFinder.setAttributeAndValue('class', 'title', 'title')
-    careerNotiFinder.setAttributeAndValue('class', 'reg_date', 'date')
-    careerNotiFinder.setAttributeAndValue('', 'href', 'href')
+    careerNotiListAll = list(ExtractedNotiList() for i in range(len(CAREER)))
+    careerNotiFinder = CarNotiFinder()
 
-    # -------------------- 공지 검색 추출 방법 설정 -------------------- #
-    # 기본값 = getText
-    genNotiFinder.setExtractionMethod("getAttr", "href")
-    genNotiFinder.extractHref = findDeptHref
-    deptNotiFinder.setExtractionMethod("getAttr", "href")
-    deptNotiFinder.extractHref = findDeptHref
-    careerNotiFinder.setExtractionMethod("getHref", "href")
+    # 생활관
+    # TODO 생활관 사이트 리뉴얼 되면 다시 작업해야 함
+    dormNotiListAll = list(ExtractedNotiList() for i in range(len(DORM)))
+    dormNotiFinder = DormNotiFinder()
 
-    # -------------------- 공지 추출 시 하위 태그 삭제 (optional) -------------------- #
-    # deptNotiFinder.notiFinderElements["title"].removeTagKeywords.append("ul")
+    # 학부 공지
+    deptNotiListAll = list(ExtractedNotiList() for i in range(DEPINT))
+    deptNotiFinder = DeptNotiFinder()
+
+    # 융합학부
+    # TODO 공합융합학부는 나중에 선택자만 공지 받을 수 있게 변경하기
+    smeNotiListAll = list(ExtractedNotiList() for i in range(len(SMEWEB)))
+    smeNotiFinder = SmeNotiFinder()
+
+    # -------------------- 이미지 링크 수정 -------------------- #
+    for notiList in careerNotiListAll:
+        notiList.linkForFixImg = CAREERAPPEND[0]
 
     # -------------------- 공지 스크랩 -------------------- #
     # 일반 공지 스크랩
-    webScrap(genNotiFinder, genNotiListAll, GENWEB, GENWEBDICT)
+    genNotiFinder.webScrap(genNotiListAll, GENWEB, GENWEBDICT)
 
     # 취업 공지 스크랩
-    webScrap(careerNotiFinder, careerNotiListAll, CAREER, GENWEBDICT)
+    careerNotiFinder.webScrap(careerNotiListAll, CAREER, GENWEBDICT)
+
+    # 생활관 공지 스크랩
+    dormNotiFinder.webScrap(dormNotiListAll, DORM, GENWEBDICT)
 
     # 학과 공지 스크랩
-    webScrap(deptNotiFinder, deptNotiListAll, DEPWEB, DEPWEBDICT)
+    deptNotiFinder.webScrap(deptNotiListAll, DEPWEB, DEPWEBDICT)
+
+    # 공학융합 공지 스크랩
+    smeNotiFinder.webScrap(smeNotiListAll, SMEWEB, GENWEBDICT)
 
     # -------------------- href 수정 -------------------- #
     # href 수정
     # 취업 공지를 제외한 나머지 학교 / 학과 공지는 BoardId 값 따로 추출 필요
+
     for genNotiList, genWeb, appendAttr in zip(genNotiListAll, GENWEB, GENAPPARY):
-        addWebPageLinkToHrefList(genNotiList, appendHref(genWeb, appendAttr))
+        KauNotiMaker.addWebPageLinkToHrefList(genNotiList, genWeb + appendAttr)
     for deptNotiList, depWeb, appendAttr in zip(deptNotiListAll, DEPWEB, DEPAPPARY):
-        addWebPageLinkToHrefList(deptNotiList, appendHref(depWeb, appendAttr))
+        KauNotiMaker.addWebPageLinkToHrefList(deptNotiList, depWeb + appendAttr)
+    for smeNotiList, smeWeb in zip(smeNotiListAll, SMEWEB):
+        KauNotiMaker.addWebPageLinkToHrefList(smeNotiList, SMEAPPEND[0])
+    KauNotiMaker.addWebPageLinkToHrefList(dormNotiListAll[0], DORM[0])
     # 취업 공지는 BoardId 값 추출 필요 없음
-    addWebPageLinkToHrefList(careerNotiListAll[0], CAREERAPPEND[0])
+    KauNotiMaker.addWebPageLinkToHrefList(careerNotiListAll[0], CAREERAPPEND[0])
 
     # -------------------- 공지 내용 미리보기 만들기 -------------------- #
-    genNotiFinder.setAttributeAndValue("id", "divViewConts", "preview")
     # genNotiListAll[GENWEB.index(DORM)].linkForFixImg = HOMEPAGE
 
-    careerNotiFinder.setAttributeAndValue("data-role", "wysiwyg-content", "preview")
-
-    deptNotiFinder.setAttributeAndValue("id", "divViewConts", "preview")
-
     for notiList in genNotiListAll:
-        extractContentsInsideLink(notiList, genNotiFinder)
+        KauNotiMaker.extractContentsInsideLink(notiList, genNotiFinder)
 
     for notiList in deptNotiListAll:
-        extractContentsInsideLink(notiList, deptNotiFinder)
+        KauNotiMaker.extractContentsInsideLink(notiList, deptNotiFinder)
 
     for notiList in careerNotiListAll:
-        extractContentsInsideLink(notiList, careerNotiFinder)
+        KauNotiMaker.extractContentsInsideLink(notiList, careerNotiFinder)
+
+    for notiList in smeNotiListAll:
+        KauNotiMaker.extractContentsInsideLink(notiList, smeNotiFinder)
+
+    for notiList in dormNotiListAll:
+        KauNotiMaker.extractContentsInsideLink(notiList, dormNotiFinder)
 
     # -------------------- 별도 notiFinder 로 생성한 공지 append (optional) -------------------- #
     # 취업 공지를 일반 공지로 넘기기
     genNotiListAll.append(careerNotiListAll[0])
 
+    # 공학융합 공지 넘기기
+    for smeNotiList in smeNotiListAll:
+        genNotiListAll.append(smeNotiList)
+
+    # 기숙사 공지 넘기기
+    genNotiListAll.append(dormNotiListAll[0])
     # -------------------- html 인스턴스에 공지, 정보 태그 추가 및 파일 저장 -------------------- #
     # 받은 공지를 html 인스턴스로 넘기기
     htmlBaseInString = HTMLBASE
@@ -116,22 +107,22 @@ def main():
 
     # 학교 일반 공지 전체 추가
     for singleCategoryNotiList in genNotiListAll:
-        addCategoryNotiToHtml(singleCategoryNotiList, htmlBase)
+        NotiMaker.addCategoryNotiToHtml(singleCategoryNotiList, htmlBase)
 
     # 학과 공지
     for singleCategoryNotiList, filename in zip(deptNotiListAll, DEPFIARY):
         # 0. 공지가 없는 경우 루프 패스 / 이건 왜 안 넣었지
-        if not isBodyTagContainsElements(htmlBase) and singleCategoryNotiList.numOfNoti == 0:
+        if not NotiMaker.isBodyTagContainsElements(htmlBase) and singleCategoryNotiList.numOfNoti == 0:
             continue
         # 1. copy 를 이용하여 일반 공지를 담은 htmlBase 를 유지한 채 htmlToSaveDeptNoti 에 htmlBase 내용 복사
         #    n 번째 루프에서는 덮어쓰기
         htmlToSaveDeptNoti = copy.copy(htmlBase)
 
         # 2. 한 학과의 공지를 htmlToSaveDeptNoti 에 저장
-        addCategoryNotiToHtml(singleCategoryNotiList, htmlToSaveDeptNoti)
+        NotiMaker.addCategoryNotiToHtml(singleCategoryNotiList, htmlToSaveDeptNoti)
 
         # 3. INFOTAG 추가
-        addInfoTag(INFOTAG, htmlToSaveDeptNoti)
+        NotiMaker.addInfoTag(INFOTAG, htmlToSaveDeptNoti)
 
         # 4. htmlBase 를 학과 파일로 저장
-        htmlToFile(htmlToSaveDeptNoti, FILEPATH, filename)
+        NotiMaker.htmlToFile(htmlToSaveDeptNoti, FILEPATH, filename)
