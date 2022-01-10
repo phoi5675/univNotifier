e33c3a967a38190258bec2c249f679b2c5ae10d8
phoi5675 <phoi5675@gmail.com>
2022-01-10 22:45:16 +0900

    update dockerfile; I HATE CRLF :(

--- scripts/scrap.sh
+++ scripts/scrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cronfile="/var/log/cron.log"
+cron_log="/var/log/cron.log"
 
 # Change CRLF -> LF
-sed -i -e 's/\r$//' *.*
+RUN sed -i -e 's/\r$//' ${SCRAPDIR}/*.*
 
 # remove previous scraped files
 find ${SCRAPDIR}/ -type f -name "*.html" -exec rm {} \;
-echo $(date) : started scraping >> ${cronfile}
+echo $(date) : started scraping >> ${cron_log}
 
 # start web scraping
 python3 ${SCRAPDIR}/run.py
-echo $(date) : finished scraping >> ${cronfile}
+echo $(date) : finished scraping >> ${cron_log}
